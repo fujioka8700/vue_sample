69ea8d1bd3d57ea1a74e9696c544d96c86aa1052
fujioka8700 <fujioka8700@gmail.com>
2021-09-17 11:59:52 +0900

    ラジオボタンまで。

---
+++ js/app4.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        subject: {
+            japanese: true,
+            english: false,
+            math: false
+        },
+        foods: ['🍩']
+    }
+});
