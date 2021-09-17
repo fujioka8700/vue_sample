f8c5c5d9c1ba29db295c9048886e306f8fc4019f
fujioka8700 <fujioka8700@gmail.com>
2021-09-17 10:56:17 +0900

    v-modelの途中

--- js/app.js
+++ js/app.js
@@ -3,6 +3,16 @@ var app = new Vue({
     data: {
         messages: {
             count: 3
-        }
+        },
+        user: {
+            gender: 'male'
+        },
+        current: 3,
+        frameworks: [
+            { name: '田中太郎', age: 19, profession: '大学生' },
+            { name: '山田花子', age: 29, profession: 'OL' },
+            { name: '一堂令', age: 16, profession: '高校生' }
+        ],
+        gifId: 52
     }
 });
