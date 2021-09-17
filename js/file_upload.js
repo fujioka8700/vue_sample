f9c1939bc4e89e8911ec2a86e3de46c15841b73c
fujioka8700 <fujioka8700@gmail.com>
2021-09-17 13:45:08 +0900

    フォーム入力バインディング、終わり

--- js/file_upload.js
+++ js/file_upload.js
@@ -1,6 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        lang: 'php'
+        file: ''
     }
 });
+
+function handleFileSelect(event) {
+    var files = event.target.files
+
+    console.log(files);
+}
+
+var elem = document.getElementById('upload');
+elem.addEventListener('change', handleFileSelect, false)
