2eca0ae807a14d9a21939c6ecf886c0c414a4e9f
fujioka8700 <fujioka8700@gmail.com>
2021-09-30 21:16:22 +0900

    [fix] 数当てゲームの続き

---
+++ src/components/NumberGuess.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h1>数字当てゲーム</h1>
+        <button @click="onClick">start</button>
+        <div>
+            <input v-if="answer > 0"
+                type="number"
+                min="0"
+                max="100"
+                v-model.number="num"
+                @keyup="guess"
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tryCountLeft: 10,
+                answer: 0,
+                num: 0
+            }
+        },
+        methods: {
+            onClick() {
+                this.answer = 10
+                this.$emit('start', this.tryCountLeft)
+            },
+            guess() {
+                // 当たり！
+                if (this.num === this.answer) {
+                    this.$emit('collect')
+                }
+
+                // もっと大きいです
+                if (this.num < this.answer) {
+                    this.$emit('higher')
+                }
+
+                // もっと小さいです
+                if (this.num > this.answer) {
+                    this.$emit('lower')
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/index.js
+++ src/index.js
@@ -2,30 +2,57 @@ import Vue from 'vue'
 import Hello from './components/Hello.vue'
 import AnchorLink from './components/AnchorLink.vue'
 import EventButtons from './components/EventButtons.vue'
+import NumberGuess from './components/NumberGuess.vue'
 import "./style.scss"
 
+// new Vue({
+//   el: '#app',
+//   data: {
+//     arguments: []
+//   },
+//   components: {
+//     hello: Hello,
+//     AnchorLink,
+//     EventButtons
+//   },
+//   methods: {
+//     onEventOne() {
+//       alert('Event 1')
+//     },
+//     onEventTwo(argument) {
+//       this.arguments = []
+//       this.arguments.push(argument)
+//     },
+//     onEventTree(arg1, arg2) {
+//       this.arguments = []
+//       this.arguments.push(arg1)
+//       this.arguments.push(arg2)
+//     }
+//   }
+// })
+
 new Vue({
-  el: '#app',
+  el: '#app2',
   data: {
-    arguments: []
+    message: 'スタートボタンを押してください',
+    tryCountLeft: 0
   },
   components: {
-    hello: Hello,
-    AnchorLink,
-    EventButtons
+    NumberGuess
   },
   methods: {
-    onEventOne() {
-      alert('Event 1')
+    onStart(tryCountLeft) {
+      this.tryCountLeft = tryCountLeft
+      this.message = 'スタート！ 🚀'
     },
-    onEventTwo(argument) {
-      this.arguments = []
-      this.arguments.push(argument)
+    onCollect() {
+      this.message = '当たり！ 🎉'
     },
-    onEventTree(arg1, arg2) {
-      this.arguments = []
-      this.arguments.push(arg1)
-      this.arguments.push(arg2)
+    onHigher() {
+      this.message = 'もっと大きいです ️👆'
+    },
+    onLower() {
+      this.message = 'もっと小さいです 👇'
     }
   }
 })
